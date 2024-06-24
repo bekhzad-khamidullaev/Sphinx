db31e497daf868942ee2b8cbb87490028f90313a
Bekhzad Khamidullaev <bekhzad_94@icloud.com>
2024-06-24 11:13:59 +0500

    commit

--- user_profiles/models.py
+++ user_profiles/models.py
@@ -4,11 +4,11 @@ from django.conf import settings
 
 class User(AbstractUser):
     # profile = models.OneToOneField('UserProfile', on_delete=models.CASCADE, related_name='user_profile', null=True, blank=True)
-    image = models.ImageField(default='', upload_to='profile_pics/')
+    image = models.ImageField(default='profile_pics/user.svg', upload_to='profile_pics/')
 
 class UserProfile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='user_profile')
-    image = models.ImageField(default='', upload_to='profile_pics/')
+    # image = models.ImageField(default='profile_pics/user.svg', upload_to='profile_pics/')
 
     def __str__(self):
         return self.user.username
--- user_profiles/views.py
+++ user_profiles/views.py
@@ -1,30 +1,32 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
 from django.contrib import messages
+from django.utils.translation import gettext_lazy as _
+from django.views.decorators.csrf import csrf_protect
 
 
+@csrf_protect
 def base(request):
     if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
+        username = request.POST.get('username')
+        password = request.POST.get('password')
         user = authenticate(request, username=username, password=password)
 
         if user is not None:
             auth_login(request, user)
-            messages.success(request, 'Successfully logged in!')
+            messages.success(request, _('Successfully logged in!'))
             return redirect('base')
         else:
-            messages.error(request, 'Invalid credentials. Please try again!')
+            messages.error(request, _('Invalid credentials. Please try again!'))
 
-    else:
-        return render(request, 'base.html')
-
+    return render(request, 'base.html')
 
 
 def user_login(request):
     return redirect('base')
 
+
 def user_logout(request):
     auth_logout(request)
-    messages.success(request, 'Successfully logged out!')
+    messages.success(request, _('Successfully logged out!'))
     return redirect('base')
