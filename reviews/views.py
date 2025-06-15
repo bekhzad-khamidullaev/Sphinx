c81d2c88c6d67d9e9dd103268eb751a6b2b8b656
Bekhzad Khamidullaev <69056685+bekhzad-khamidullaev@users.noreply.github.com>
2025-06-16 04:56:17 +0500

    Add reviews app and update tests

---
+++ reviews/views.py
@@ -0,0 +1,8 @@
+from rest_framework import viewsets, permissions
+from .models import Review
+from .serializers import ReviewSerializer
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+    permission_classes = [permissions.AllowAny]
